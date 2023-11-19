4c3e9fb80d8be7848d9cc111c9bf2e6fc58b1201
Delziron Braz <44593154+DelzironBraz@users.noreply.github.com>
2023-11-19 18:08:20 -0300

    feat: create mui theme (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,25 @@ import './App.css';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import Profile from './pages/Profile/Profile';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { themeSettings } from './theme';
 
 const App = () => {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
-    <Routes>
-      <Route path='/' element={<Login />} />
-      <Route path='profile/:userId' element={<Profile />} />
-      <Route path="/feed" element={<Home />} />
-    </Routes>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='profile/:userId' element={<Profile />} />
+        <Route path="/feed" element={<Home />} />
+      </Routes>
+    </ThemeProvider>
   )
 };
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,38 @@
+import { useState } from "react";
+import {
+    Box,
+    IconButton,
+    InputBase,
+    Typography,
+    Select,
+    MenuItem,
+    FormControl,
+    useTheme,
+    useMediaQuery
+} from '@mui/material';
+import {
+    Search,
+    Message,
+    DarkMode,
+    LightMode,
+    Notifications,
+    Help,
+    Menu,
+    Close
+} from '@mui/icons-material';
+import { setMode, setLogout } from "../../state";
+import { useNavigate } from "react-router-dom";
+import FlexBetween from "../FlexBetween/FlexBetween";
+import { useDispatch, useSelector } from "react-redux";
+
 const Navbar = () => {
+    const [isMobileMenuToggled, setIsModebileMenuToggled] = useState(false);
+    const dispach = useDispatch();
+    const navigate = useNavigate();
+    const user = useSelector((state) => state.user);
+    const isNonMobileScreens = useMediaQuery('(min-width: 1000px)');
+    const theme = useTheme();
+
     return (
         <h1>Hello Navbar</h1>
     );
